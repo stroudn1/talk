80ab7bc44f6e30c4e0c50838177c4e1a4e2d7f81
tessalt <tessathornton@gmail.com>
2020-03-19 11:10:35 -0400

    toggle soiler display

--- src/core/client/stream/common/HTMLContent/HTMLContent.tsx
+++ src/core/client/stream/common/HTMLContent/HTMLContent.tsx
@@ -1,14 +1,14 @@
 import cn from "classnames";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useRef } from "react";
 
-// import { createPurify } from "coral-common/utils/purify";
+import { createPurify } from "coral-common/utils/purify";
 
 import styles from "./HTMLContent.css";
 
 /**
  * Create a purify instance that will be used to handle HTML content.
  */
-// const purify = createPurify(window, false);
+const purify = createPurify(window, false);
 
 interface HTMLContentProps {
   children: string;
@@ -18,11 +18,24 @@ interface HTMLContentProps {
 const HTMLContent: FunctionComponent<HTMLContentProps> = ({
   children,
   className,
-}) => (
-  <div
-    className={cn(styles.root, className)}
-    dangerouslySetInnerHTML={{ __html: children }}
-  />
-);
+}) => {
+  const div = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (div && div.current) {
+      div.current.addEventListener("click", e => {
+        if (e.target && (e.target as Element).matches(".coral-spoiler")) {
+          e.target.classList.toggle("coral-spoiler-open");
+        }
+      });
+    }
+  }, [children]);
+  return (
+    <div
+      ref={div}
+      className={cn(styles.root, className)}
+      dangerouslySetInnerHTML={{ __html: purify.sanitize(children) }}
+    />
+  );
+};
 
 export default HTMLContent;
